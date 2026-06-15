5c2f4e1e8c4eb5112c8407e3ece6ac33cebfc46d
AravKotwal18 <aravkotwal1604@gmail.com>
2026-06-15 21:58:51 +0530

    Add BookAPI project files

--- obj/Debug/net10.0/BookApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/BookApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed2f0633d295d5333b1091bdea829e0a745b03c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bee5a5900f5a0713df16833f13f7ac2215f1f0af")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
